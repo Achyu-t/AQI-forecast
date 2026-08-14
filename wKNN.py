5464500bf48ec0f71658ab6a6e9d25501b22a5e5
Achyu-t <acchyutbhattarai@gmail.com>
2026-08-14 16:14:44 +0545

    Progress till now

--- wKNN.py
+++ wKNN.py
@@ -1,7 +1,30 @@
 import numpy as np
 from collections import Counter
 
-def distance ( latitude , longitude): 
 
-    lat_deg = np.radians(latitude)
-    long_deg = np.radians(longitude)
+def weighted_euclidean_distance(p,q) :
+
+  pass
+
+class KNearestNeighbors:
+
+  def __init__(self, k =3):
+    self.k = k
+    self.points = None
+
+     
+  def fit(self):
+    self.points = points
+    return self
+
+  def predict(self , new_points) :
+
+    distances = [[euclidean_distance(point , new_points), category] for category in self.points for point in self.points[category]]
+
+    labels = [category[1] for category in sorted(distances)[:self.k]]
+
+    result = Counter(labels).most_common(1)[0][0]
+
+    return result
+
+  
